e73a01979ecf57a3f4a8eb1169264a9de69362bf
sensebecommon <freean2468@gmail.com>
2021-03-17 09:37:17 +0900

    Initial commit

---
+++ android/app/src/main/java/com/mirae/mp3loud/ActivityMain.kt
@@ -0,0 +1,11 @@
+package com.mirae.mp3loud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
