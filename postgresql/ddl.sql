98342063ac5951773a6f92f081add6515f143c66
sensebecommon <freean2468@gmail.com>
2021-03-18 16:45:36 +0900

    #postgre_1 : db 구현

---
+++ postgresql/ddl.sql
@@ -0,0 +1,26 @@
+drop table if exists like_table;
+drop table if exists mp3_table;
+drop table if exists user_table;
+
+create table if not exists user_table(
+	no varchar(12) not null unique,
+	primary key(no)
+);
+
+create table if not exists mp3_table(
+	id serial not null unique,
+	played_times bigint not null default 0,
+	title varchar(30) not null,
+	artist varchar(30) not null,
+	play_length_in_sec int not null,
+	sample bytea,
+	origin bytea,
+	constraint pk_id primary key(id)
+);
+
+create table if not exists like_table(
+	no varchar(12) not null,
+	id int not null,
+	constraint fk_no foreign key(no) references user_table(no) on delete cascade on update cascade,
+	constraint fk_id foreign key(id) references mp3_table(id) on delete cascade on update cascade
+);
