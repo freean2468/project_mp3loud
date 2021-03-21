1c3ccc256596a9fff89cc985c6cdb97b1c9415a4
freean <freean2468@gmail.com>
2021-03-22 00:59:21 +0900

    #android_10 : drawer 구조 삭제

--- android/app/src/main/java/com/mirae/mp3loud/activity/ActivityMain.java
+++ android/app/src/main/java/com/mirae/mp3loud/activity/ActivityMain.java
@@ -1,7 +1,6 @@
 package com.mirae.mp3loud.activity;
 
 import androidx.annotation.NonNull;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -14,64 +13,14 @@ import com.mirae.mp3loud.R;
 import com.mirae.mp3loud.fragment.Fragment01;
 import com.mirae.mp3loud.fragment.Fragment02;
 
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 public class ActivityMain extends FragmentActivity {
     public static ViewPager2 viewPager2;
     private Adapter viewPagerAdapter;
-
-    private DrawerLayout drawerLayout;
-    private View drawerView;
-    private Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        drawerLayout=findViewById(R.id.drawerLayout);
-        drawerView= findViewById(R.id.drawer);
-        btnClose= findViewById(R.id.btnClose);
-
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.closeDrawers();
-            }
-        });
-
-        drawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(@NonNull View view, float v) {
-                //슬라이드 진행되었을 때 발생된 콜백함수
-            }
-
-            @Override
-            public void onDrawerOpened(@NonNull View view) {
-                Toast.makeText(ActivityMain.this, "열렸다 5반",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onDrawerClosed(@NonNull View view) {
-                Toast.makeText(ActivityMain.this, "닫혔.5반",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onDrawerStateChanged(int i) {
-                //슬라이드 상태가 변화되었을 때 발생하는 콜백함수
-            }
-        });
-
-        drawerView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-
-                return true;
-            }
-        });
 
         viewPager2 = findViewById(R.id.viewPager);
         viewPagerAdapter = new Adapter(this);
--- android/app/src/main/java/com/mirae/mp3loud/fragment/Fragment01.java
+++ android/app/src/main/java/com/mirae/mp3loud/fragment/Fragment01.java
@@ -5,16 +5,20 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mirae.mp3loud.R;
+import com.mirae.mp3loud.activity.ActivityMain;
 import com.mirae.mp3loud.adapter.AdapterPlayList;
 import com.mirae.mp3loud.object.ObjectVolley;
 
